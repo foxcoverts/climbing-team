041be2c4a1637dc7575b7e69f50fe3dffb1eea6c
Phill Sparks <me@phills.me.uk>
2024-02-03 22:28:04 +0000

    Add styles to select input, esp dark

--- resources/views/components/select-input.blade.php
+++ resources/views/components/select-input.blade.php
@@ -7,7 +7,11 @@
     }
 @endphp
 
-<select {{ $attributes }}>
+<select {!! $attributes->merge([
+    'class' =>
+        'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm',
+    'style' => 'color-scheme: light dark',
+]) !!}>
     @foreach ($options::cases() as $option)
         <option value="{{ $option->value }}" @selected($value == $option)>{{ __($option->name) }}</option>
     @endforeach
