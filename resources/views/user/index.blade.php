fa0d2439440695dfccb1c70fc097ea2788ca3001
Phill Sparks <me@phills.me.uk>
2024-03-01 22:04:05 +0000

    Make users list a table

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,3 +1,4 @@
+@use('App\Enums\Accreditation')
 @use('App\Models\User')
 <x-layout.app :title="__('Users')">
     <section class="p-4 sm:p-8">
@@ -7,8 +8,48 @@
             </h2>
         </header>
 
-        <div class="grid grid-cols-1 divide-y divide-gray-200 border-y border-gray-200 my-2">
-            @each('user.partials.item', $users, 'user', 'user.partials.empty')
-        </div>
+        <table class="w-full mt-6 text-gray-700 dark:text-gray-300 ">
+            <thead>
+                <tr>
+                    <th
+                        class="px-1 text-left sticky top-0 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-300">
+                        Name</th>
+                    <th class="px-1 text-center text-nowrap sticky top-0 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-300"
+                        width="1">Role</th>
+                    @foreach (Accreditation::cases() as $accreditation)
+                        <th @class([
+                            'px-1 text-center text-nowrap sticky top-0 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-300',
+                            'hidden md:table-cell' => in_array($accreditation, [
+                                Accreditation::ManageUsers,
+                                Accreditation::ManageBookings,
+                            ]),
+                        ]) width="1">
+                            {{ __("app.user.accreditation.{$accreditation->value}") }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 border-y border-gray-200">
+                @foreach ($users as $user)
+                    <tr class="hover:bg-gray-100 hover:dark:text-gray-200 dark:hover:bg-gray-700 cursor-pointer"
+                        @click="window.location='{{ route('user.show', $user) }}'">
+                        <td class="px-1"><a href="{{ route('user.show', $user) }}">{{ $user->name }}</a></td>
+                        <td class="px-1 text-center"><x-badge.role :role="$user->role" class="text-sm text-nowrap" /></td>
+                        @foreach (Accreditation::cases() as $accreditation)
+                            <td @class([
+                                'px-1',
+                                'hidden md:table-cell' => in_array($accreditation, [
+                                    Accreditation::ManageUsers,
+                                    Accreditation::ManageBookings,
+                                ]),
+                            ])>
+                                @if ($user->accreditations->contains($accreditation))
+                                    <x-badge.accreditation :accreditation="$accreditation" class="text-sm text-nowrap" />
+                                @endif
+                            </td>
+                        @endforeach
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </section>
 </x-layout.app>
