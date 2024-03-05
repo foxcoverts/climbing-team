f3e250ba586812ec28a3533b065fe2e028f41168
Phill Sparks <me@phills.me.uk>
2024-03-05 20:18:25 +0000

    Disable booking form when booking is cancelled

--- resources/views/booking/edit.blade.php
+++ resources/views/booking/edit.blade.php
@@ -1,7 +1,14 @@
 @use('App\Enums\BookingStatus')
 <x-layout.app :title="__('Edit Booking')">
-    <section class="p-4 sm:p-8" x-data='{
-        booking: {},
+    <section class="p-4 sm:p-8"
+        x-data='{
+        booking: {
+            cancelled: {{ $booking->isCancelled() ? 'true' : 'false' }},
+        },
+        updateCancelled(ev) {
+            this.booking.cancelled = !ev.target.checked;
+            if (this.booking.cancelled) $refs.form.reset();
+        }
     }'>
         <header>
             <h2 class="text-3xl font-medium text-gray-900 dark:text-gray-100 flex flex-wrap gap-2">
@@ -22,11 +29,54 @@
                     <x-badge.booking-status :status="$booking->status" class="text-sm" />
                 </p>
 
-                <form method="post" action="{{ route('booking.update', $booking) }}" id="update-booking"
+                <form method="post" action="{{ route('booking.update', $booking) }}" id="update-booking" x-ref="form"
                     class="space-y-6 max-w-xl mb-6">
                     @csrf
                     @method('PATCH')
 
+                    @if ($booking->isCancelled())
+                        <div class="space-y-1">
+                            <span class="font-bold after:content-[':']">{{ __('Restore Booking') }}</span>
+                            <p>
+                                {{ __('This booking has been cancelled. If you restore this booking you will need to find instructors and confirm the booking again. If you do not want to invite any of the previous attendees you should remove them from the booking first.') }}
+                            </p>
+                            <label class="mt-1 w-full flex space-x-1 items-center">
+                                <input type="checkbox" id="status" name="status"
+                                    value="{{ BookingStatus::Tentative }}" x-model.fill="booking.status"
+                                    @change="updateCancelled" />
+                                <span>{{ __('Restore booking') }}</span>
+                            </label>
+                            <x-input-error :messages="$errors->get('status')" />
+                        </div>
+                    @elseif ($booking->isTentative())
+                        <div class="space-y-1">
+                            <span class="font-bold after:content-[':']">{{ __('Confirm Booking') }}</span>
+                            @if ($instructors_attending->isEmpty() && !auth()->user()->isTeamLeader())
+                                <p>
+                                    {{ __('You can only confirm a booking when there is an instructor attending.') }}
+                                </p>
+                            @else
+                                <p>
+                                    {{ __('Before you confirm this booking you should ensure that there are enough instructors attending. It is also recommended that you have chosen a ') }}
+                                    <a href="#lead_instructor_id">{{ __('Lead Instructor') }}</a>.
+                                </p>
+                                <label class="mt-1 w-full flex space-x-1 items-center">
+                                    <input type="checkbox" id="status" name="status"
+                                        value="{{ BookingStatus::Confirmed }}" />
+                                    <span>{{ __('Confirm booking') }}</span>
+                                </label>
+                            @endif
+                        </div>
+                    @else
+                        {{-- Booking is confirmed --}}
+                        <div class="space-y-1">
+                            <span class="font-bold after:content-[':']">{{ __('Confirm Booking') }}</span>
+                            <label class="mt-1 w-full flex space-x-1 items-center">
+                                <input type="checkbox" name="_status" checked disabled required />
+                                <span>{{ __('This booking has been confirmed.') }}</span>
+                            </label>
+                        </div>
+                    @endif
                     <div class="flex gap-6" x-data="{
                         start_time: '',
                         end_time: '',
@@ -66,7 +116,8 @@
                         <div class="space-y-1">
                             <x-input-label for="start_date" :value="__('Date')" />
                             <x-text-input id="start_date" name="start_date" type="date" :value="old('start_date', $booking->start_date)"
-                                placeholder="yyyy-mm-dd" required x-model.fill="booking.start_date" />
+                                placeholder="yyyy-mm-dd" required x-model.fill="booking.start_date"
+                                x-bind:disabled="booking.cancelled" />
                             <x-input-error :messages="$errors->get('start_date')" />
                         </div>
 
@@ -74,14 +125,15 @@
                             <x-input-label for="start_time" :value="__('Start')" />
                             <x-text-input id="start_time" name="start_time" type="time" step="60"
                                 :value="old('start_time', $booking->start_time)" placeholder="hh:mm" required x-model.fill="start_time"
-                                @change="syncEndTime" />
+                                @change="syncEndTime" x-bind:disabled="booking.cancelled" />
                             <x-input-error :messages="$errors->get('start_time')" />
                         </div>
 
                         <div class="space-y-1">
                             <x-input-label for="end_time" :value="__('End')" />
-                            <x-text-input id="end_time" name="end_time" type="time" step="60" :value="old('end_time', $booking->end_time)"
-                                placeholder="hh:mm" required x-model.fill="end_time" @blur="syncDuration" />
+                            <x-text-input id="end_time" name="end_time" type="time" step="60"
+                                :value="old('end_time', $booking->end_time)" placeholder="hh:mm" required x-model.fill="end_time"
+                                @blur="syncDuration" x-bind:disabled="booking.cancelled" />
                             <x-input-error :messages="$errors->get('end_time')" />
                         </div>
                     </div>
@@ -89,7 +141,8 @@
                     <div class="space-y-1">
                         <x-input-label for="location" :value="__('Location')" />
                         <x-text-input id="location" name="location" type="text" class="block w-full"
-                            :value="old('location', $booking->location)" maxlength="255" required x-model.fill='booking.location' />
+                            :value="old('location', $booking->location)" maxlength="255" required x-model.fill='booking.location'
+                            x-bind:disabled="booking.cancelled" />
                         <x-input-error :messages="$errors->get('location')" />
                     </div>
 
@@ -133,65 +186,23 @@
                         <x-input-label for="activity" :value="__('Activity')" />
                         <x-text-input id="activity" name="activity" type="text" class="block w-full"
                             :value="old('activity', $booking->activity)" maxlength="255" required autocomplete="on" list="activity-suggestions"
-                            x-model.fill="booking.activity" />
+                            x-model.fill="booking.activity" x-bind:disabled="booking.cancelled" />
                         <x-input-error :messages="$errors->get('activity')" />
                     </div>
 
                     <div class="space-y-1">
                         <x-input-label for="group_name" :value="__('Group Name')" />
                         <x-text-input id="group_name" name="group_name" type="text" class="block w-full"
-                            :value="old('group_name', $booking->group_name)" maxlength="255" required />
+                            :value="old('group_name', $booking->group_name)" maxlength="255" required x-bind:disabled="booking.cancelled" />
                         <x-input-error :messages="$errors->get('group_name')" />
                     </div>
 
                     <div class="space-y-1">
                         <x-input-label for="notes" :value="__('Notes')" />
-                        <x-textarea id="notes" name="notes" class="block w-full" :value="old('notes', $booking->notes)" />
+                        <x-textarea id="notes" name="notes" class="block w-full" :value="old('notes', $booking->notes)"
+                            x-bind:disabled="booking.cancelled" />
                         <x-input-error :messages="$errors->get('notes')" />
                     </div>
-
-                    @if ($booking->isCancelled())
-                        <div class="space-y-1">
-                            <span class="font-bold after:content-[':']">{{ __('Restore Booking') }}</span>
-                            <p>
-                                {{ __('This booking has been cancelled. If you restore this booking you will need to find instructors and confirm the booking again. If you do not want to invite any of the previous attendees you should remove them from the booking first.') }}
-                            </p>
-                            <label class="mt-1 w-full flex space-x-1 items-center">
-                                <input type="checkbox" id="status" name="status"
-                                    value="{{ BookingStatus::Tentative }}" />
-                                <span>{{ __('Restore booking') }}</span>
-                            </label>
-                            <x-input-error :messages="$errors->get('status')" />
-                        </div>
-                    @elseif ($booking->isTentative())
-                        <div class="space-y-1">
-                            <span class="font-bold after:content-[':']">{{ __('Confirm Booking') }}</span>
-                            @if ($instructors_attending->isEmpty() && !auth()->user()->isTeamLeader())
-                                <p>
-                                    {{ __('You can only confirm a booking when there is an instructor attending.') }}
-                                </p>
-                            @else
-                                <p>
-                                    {{ __('Before you confirm this booking you should ensure that there are enough instructors attending. It is also recommended that you have chosen a ') }}
-                                    <a href="#lead_instructor">{{ __('Lead Instructor') }}</a>.
-                                </p>
-                                <label class="mt-1 w-full flex space-x-1 items-center">
-                                    <input type="checkbox" id="status" name="status"
-                                        value="{{ BookingStatus::Confirmed }}" />
-                                    <span>{{ __('Confirm booking') }}</span>
-                                </label>
-                            @endif
-                        </div>
-                    @else
-                        {{-- Booking is confirmed --}}
-                        <div class="space-y-1">
-                            <span class="font-bold after:content-[':']">{{ __('Confirm Booking') }}</span>
-                            <label class="mt-1 w-full flex space-x-1 items-center">
-                                <input type="checkbox" checked disabled required />
-                                <span>{{ __('This booking has been confirmed. No further status changes can be made.') }}</span>
-                            </label>
-                        </div>
-                    @endif
                 </form>
             </div>
 
@@ -199,7 +210,7 @@
         </div>
 
         <footer class="flex items-center gap-4 mt-6">
-            <x-button.primary form="update-booking">
+            <x-button.primary form="update-booking" x-bind:disabled="booking.cancelled">
                 {{ __('Update') }}
             </x-button.primary>
             @include('booking.partials.delete-button')
