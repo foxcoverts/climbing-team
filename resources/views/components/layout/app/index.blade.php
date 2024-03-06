c2347712135f980d93d5ea15d82ad20ae50fa20f
Phill Sparks <me@phills.me.uk>
2024-03-06 14:08:11 +0000

    Fix missing title in app layout

--- resources/views/components/layout/app/index.blade.php
+++ resources/views/components/layout/app/index.blade.php
@@ -1,4 +1,4 @@
-@props(['assets' => []])
+@props(['assets' => [], 'title' => ''])
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
--- resources/views/components/layout/guest/index.blade.php
+++ resources/views/components/layout/guest/index.blade.php
@@ -1,3 +1,4 @@
+@props(['title' => ''])
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
