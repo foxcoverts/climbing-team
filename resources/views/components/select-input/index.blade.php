69db2320cefe640510edbfaa29494e51fab519bf
Phill Sparks <me@phills.me.uk>
2024-03-06 10:38:27 +0000

    Add disabled: styles for select-input, text-input, and textarea

--- resources/views/components/select-input/index.blade.php
+++ resources/views/components/select-input/index.blade.php
@@ -2,7 +2,7 @@
 
 <select {!! $attributes->merge([
     'class' =>
-        'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm',
+        'border-gray-300 disabled:cursor-not-allowed disabled:text-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:disabled:text-gray-600 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm',
     'style' => 'color-scheme: light dark',
 ]) !!}>
     {{ $slot }}
