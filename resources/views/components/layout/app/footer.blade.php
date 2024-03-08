472af0ee372aa522d8cd1d32a165c51f3f354bc8
Phill Sparks <me@phills.me.uk>
2024-03-08 11:19:52 +0000

    Improve footer layout for xs screens

--- resources/views/components/layout/app/footer.blade.php
+++ resources/views/components/layout/app/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="w-full border-t lg:pl-64 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-300">
-    <div class="px-8 py-6 space-y-2 md:space-y-0 md:flex md:justify-between md:items-center">
+    <div class="px-8 py-6 flex flex-wrap justify-between">
         <p>© Copyright {{ date('Y') }}</p>
 
-        <div class="flex space-x-6">
-            <a href="#" class="hover:text-gray-900 dark:hover:text-gray-50">Terms of Service</a>
-            <a href="#" class="hover:text-gray-900 dark:hover:text-gray-50">Privacy Policy</a>
-            <a href="#" class="hover:text-gray-900 dark:hover:text-gray-50">About Us</a>
-        </div>
+        <ul class="sm:flex sm:space-x-6 space-y-2 sm:space-y-0">
+            <li><a href="#" class="hover:text-gray-900 dark:hover:text-gray-50">Terms of Service</a></li>
+            <li><a href="#" class="hover:text-gray-900 dark:hover:text-gray-50">Privacy Policy</a></li>
+            <li><a href="#" class="hover:text-gray-900 dark:hover:text-gray-50">About Us</a></li>
+        </ul>
     </div>
 </footer>
