8ead70e70ad403023093c6e9215ac8cf5d002a29
Phill Sparks <me@phills.me.uk>
2024-03-09 21:53:48 +0000

    Fix booking invite email

--- resources/views/mail/booking/invite.md.blade.php
+++ resources/views/mail/booking/invite.md.blade.php
@@ -5,35 +5,35 @@
 @if (!empty($changed_summary))
 <x-mail::panel>
 **@lang('This booking has been updated')**<br>
-**@lang('Changes') }}:** {{ $changed_summary.
+**@lang('Changes'):** {{ $changed_summary }}.
 </x-mail::panel>
 @endif
 
 @if ($status_changed != '')
-**@lang('Status') }}**{{ $status_changed<br>
+**@lang('Status')**{{ $status_changed }}<br>
 @lang("app.booking.status.{$booking->status->value}")
 @endif
 
-**@lang('When') }}**{{ $when_changed<br>
+**@lang('When')**{{ $when_changed }}<br>
 {{ $when }}
 
-**@lang('Location') }}**{{ $location_changed<br>
+**@lang('Location')**{{ $location_changed }}<br>
 {{ $booking->location }}
 
-**@lang('Activity') }}**{{ $activity_changed<br>
+**@lang('Activity')**{{ $activity_changed }}<br>
 {{ $booking->activity }}
 
 @if ($booking->lead_instructor)
-**@lang('Lead Instructor') }}**{{ $lead_instructor_changed<br>
+**@lang('Lead Instructor')**{{ $lead_instructor_changed }}<br>
 {{ $booking->lead_instructor->name }}
 @endif
 
-**@lang('Group') }}**{{ $group_changed<br>
+**@lang('Group')**{{ $group_changed }}<br>
 {{ $booking->group_name }}
 
 @if ($booking->notes)
 <x-markdown>
-**@lang('Notes') }}**{{ $notes_changed<br>
+**@lang('Notes')**{{ $notes_changed }}<br>
 {{ $booking->notes }}
 </x-markdown>
 @endif
