06b741ff921207f08caf2bef28972eaf69d289b5
Phill Sparks <me@phills.me.uk>
2024-03-09 21:47:45 +0000

    Show mail body in a readonly textarea

--- resources/views/mail-log/show.blade.php
+++ resources/views/mail-log/show.blade.php
@@ -1,16 +1,16 @@
 <x-layout.app :title="__('Mail')">
-    <section class="p-4 sm:p-8 max-w-xl space-y-4">
+    <section class="p-4 sm:p-8 space-y-4">
         <header>
             <h2 class="text-2xl sm:text-3xl font-medium">@lang('Mail')</h2>
         </header>
 
-        <div class="space-y-2 my-2 w-full max-w-xl flex-grow">
+        <div class="space-y-2 my-2 w-full flex-grow">
             <p><dfn class="block not-italic font-medium after:content-[':']">@lang('ID')</dfn>
                 {{ $mail->id }}</p>
             <p><dfn class="block not-italic font-medium after:content-[':']">@lang('Created')</dfn>
                 {{ localDate($mail->created_at) }}</p>
             <div><dfn class="block not-italic font-medium after:content-[':']">@lang('Body')</dfn>
-                <div class="font-mono whitespace-pre">{{ $mail->body }}</div>
+                <x-textarea class="w-full" readonly>{{ $mail->body }}</x-textarea>
             </div>
         </div>
 
