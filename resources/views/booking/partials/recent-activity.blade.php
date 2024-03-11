2821093921df6fdbb1f38eca508eecb06b3aefe7
Phill Sparks <me@phills.me.uk>
2024-03-11 16:17:35 +0000

    Start the recent activity with when the booking was created.

--- resources/views/booking/partials/recent-activity.blade.php
+++ resources/views/booking/partials/recent-activity.blade.php
@@ -1,18 +1,25 @@
-@if ($booking->changes->count())
-    <h3 class="text-xl font-medium">@lang('Recent Activity')</h3>
+<h3 class="text-xl font-medium">@lang('Recent Activity')</h3>
 
-    <div class="space-y-2">
-        @foreach ($booking->changes as $change)
-            <div id="{{ $change->id }}">
-                <p>{{ localDate($change->created_at)->ago() }}</p>
-                @foreach ($change->comments as $comment)
-                    <div class="border-l-2 ml-2 pl-2" id="{{ $comment->id }}">
-                        <div><a href="{{ route('user.show', $change->author) }}"
-                                class="font-medium">{{ $change->author->name }}</a> commented:</div>
-                        <p class="border-l-2 ml-2 pl-2">{{ $comment->body }}</p>
-                    </div>
-                @endforeach
-            </div>
-        @endforeach
+<div class="space-y-2">
+    <div>
+        <p><span title="{{ localDate($booking->created_at)->toDayDateTimeString() }}" class="cursor-help">
+                {{ localDate($booking->created_at)->ago() }}
+            </span></p>
+        <div class="border-l-2 ml-2 pl-2">@lang('Booking created.')</div>
     </div>
-@endif
+
+    @foreach ($booking->changes as $change)
+        <div id="{{ $change->id }}">
+            <p><span title="{{ localDate($change->created_at)->toDayDateTimeString() }}" class="cursor-help">
+                    {{ localDate($change->created_at)->ago() }}
+                </span></p>
+            @foreach ($change->comments as $comment)
+                <div class="border-l-2 ml-2 pl-2" id="{{ $comment->id }}">
+                    <div><a href="{{ route('user.show', $change->author) }}"
+                            class="font-medium">{{ $change->author->name }}</a> commented:</div>
+                    <p class="border-l-2 ml-2 pl-2">{{ $comment->body }}</p>
+                </div>
+            @endforeach
+        </div>
+    @endforeach
+</div>
