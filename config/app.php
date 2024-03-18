ae7d93df42a350ff2eccc1d5b18b021615298f81
Phill Sparks <me@phills.me.uk>
2024-03-18 14:11:39 +0000

    Remove unused `spatie/laravel-html`

--- config/app.php
+++ config/app.php
@@ -157,11 +157,6 @@ return [
 
     'providers' => ServiceProvider::defaultProviders()->merge([
         /*
-         * Package Service Providers...
-         */
-        Spatie\Html\HtmlServiceProvider::class,
-
-        /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
