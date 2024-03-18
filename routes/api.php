346aaa480040970cd6fb8f958770052388f791df
Phill Sparks <me@phills.me.uk>
2024-03-18 14:17:19 +0000

    Use more expressive middleware for api routes

--- routes/api.php
+++ routes/api.php
@@ -2,24 +2,15 @@
 
 use App\Http\Controllers\Api\BookingController;
 use App\Http\Controllers\Api\StoreMailLogController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->name('api.')->group(function () {
+Route::middleware(Authenticate::using('sanctum'))->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
-    })->name('user');
+    });
+
     Route::apiResource('/booking', BookingController::class);
 });
 
