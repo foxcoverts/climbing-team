3156380915782e5392a4bf664a165ca4dcdea075
Phill Sparks <me@phills.me.uk>
2024-03-18 13:42:35 +0000

    Use FirstUser middleware by class

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,7 +59,6 @@ class Kernel extends HttpKernel
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'firstUser' => \App\Http\Middleware\FirstUser::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
--- routes/auth.php
+++ routes/auth.php
@@ -9,10 +9,11 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Middleware\FirstUser;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::controller(RegisteredUserController::class)->middleware('firstUser')->group(function () {
+    Route::controller(RegisteredUserController::class)->middleware(FirstUser::class)->group(function () {
         Route::get('register', 'create')->name('register');
         Route::post('register', 'store');
     });
