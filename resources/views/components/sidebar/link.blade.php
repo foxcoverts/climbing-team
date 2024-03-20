02f3d617a2ca91b5cf6609847e55978395e7f601
Phill Sparks <me@phills.me.uk>
2024-03-20 21:16:01 +0000

    Use icon components for sidebar icons

--- resources/views/components/sidebar/link.blade.php
+++ resources/views/components/sidebar/link.blade.php
@@ -1,11 +1,15 @@
-@props(['route', 'matchRoutes' => null, 'icon', 'label'])
+@props(['route', 'matchRoutes' => null, 'label'])
 <a href="{{ route($route) }}" @class([
     'w-full flex items-center text-blue-400 h-10 pl-4 sm:pl-8 lg:pl-4 hover:bg-gray-200 focus:bg-gray-200 active:bg-gray-200 dark:hover:bg-gray-700 dark:focus:bg-gray-700 dark:active:bg-gray-700',
     'bg-white dark:bg-gray-800 dark:text-white' => Route::is(
         $matchRoutes ?? $route),
 ])>
-    <svg class="h-5 w-5 fill-current mr-2" viewBox="0 0 20 20">
-        {{ $icon }}
-    </svg>
+    @if ($attributes->has('icon'))
+        <x-dynamic-component :component="'icon.' . $attributes->get('icon')" class="h-5 w-5 fill-current mr-2" />
+    @else
+        <svg class="h-5 w-5 fill-current mr-2" viewBox="0 0 20 20">
+            {{ $icon }}
+        </svg>
+    @endif
     <span class="text-gray-700 dark:text-gray-200">{{ $label }}</span>
 </a>
