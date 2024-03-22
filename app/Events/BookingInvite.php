22e2381b09eebbf70caa03a8978f6349f4218d8c
Phill Sparks <me@phills.me.uk>
2024-03-22 13:04:51 +0000

    Remove unused dispatch code

--- app/Events/BookingInvite.php
+++ app/Events/BookingInvite.php
@@ -4,17 +4,11 @@ namespace App\Events;
 
 use App\Models\Booking;
 use App\Models\User;
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class BookingInvite
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use SerializesModels;
 
     /**
      * Create a new event instance.
@@ -23,17 +17,5 @@ class BookingInvite
         public Booking $booking,
         public User $attendee
     ) {
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
     }
 }
