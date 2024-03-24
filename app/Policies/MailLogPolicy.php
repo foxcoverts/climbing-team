700ebecd7808ce8c1c2d0be8413717398456a6a9
Phill Sparks <me@phills.me.uk>
2024-03-24 12:54:26 +0000

    Allow unauthenticated access to `/api/mail`

--- app/Policies/MailLogPolicy.php
+++ app/Policies/MailLogPolicy.php
@@ -27,7 +27,7 @@ class MailLogPolicy
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(?User $user): bool
     {
         return true;
     }
