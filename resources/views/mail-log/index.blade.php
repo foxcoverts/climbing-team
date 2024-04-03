2a74708b0696d8d7e9acecab4cf95bd316770640
Phill Sparks <me@phills.me.uk>
2024-04-03 18:06:46 +0100

    Improve display of mail log index on small screens

--- resources/views/mail-log/index.blade.php
+++ resources/views/mail-log/index.blade.php
@@ -1,71 +1,61 @@
 <x-layout.app :title="__('Mail log')">
-    <section class="p-4 sm:px-8">
-        <header>
-            <h2 class="text-2xl sm:text-3xl font-medium text-gray-900 dark:text-gray-100">
+    <section class="py-4">
+        <header class="px-4 sm:px-8">
+            <h1 class="text-2xl sm:text-3xl font-medium text-gray-900 dark:text-gray-100">
                 @lang('Mail log')
-            </h2>
+            </h1>
         </header>
 
-        <table class="w-full mt-6 text-gray-700 dark:text-gray-300 ">
-            <thead>
-                <tr>
-                    <th class="pl-3 sticky top-0 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-300">
-                        <x-icon.empty-outline class="w-5 h-5 fill-current" />
-                    </th>
-                    <th
-                        class="px-3 py-2 text-left text-nowrap sticky top-0 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-300">
-                        @lang('To')</th>
-                    <th
-                        class="px-3 py-2 text-left text-nowrap sticky top-0 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-300">
-                        @lang('From')</th>
-                    <th
-                        class="px-3 py-2 text-left text-nowrap sticky top-0 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-300">
-                        @lang('Received')</th>
-                </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-200 border-y border-gray-200">
-                @forelse ($mails as $mail)
-                    <tr class="hover:bg-gray-100 hover:dark:text-gray-200 dark:hover:bg-gray-700 cursor-pointer"
-                        @click="window.location='{{ route('mail.show', $mail) }}'">
-                        <td @class(['pl-3', 'text-sky-500' => $mail->isUnread()])>
+        <div class="text-gray-700 dark:text-gray-300 divide-y border-y mt-4">
+            @forelse ($mails as $mail)
+                <div class="p-2 px-4 sm:px-8 flex items-start gap-2 hover:bg-gray-100 hover:dark:text-gray-200 dark:hover:bg-gray-700 cursor-pointer"
+                    @click="window.location='{{ route('mail.show', $mail) }}'">
+                    <div>
+                        <p @class(['py-0.5', 'text-sky-500' => $mail->isUnread()])>
                             @if ($mail->done)
                                 <x-icon.checkmark-outline class="w-5 h-5 fill-current" />
                             @else
-                                <x-icon.question-outline class="w-5 h-5 fill-current" />
+                                <x-icon.empty-outline class="w-5 h-5 fill-current" />
                             @endif
-                        </td>
-                        <td class="px-3 py-3">
+                        </p>
+                    </div>
+                    <div class="space-y-2 overflow-hidden">
+                        <p class="flex items-center space-x-1">
+                            <dfn class="not-italic font-medium">@lang('To'):</dfn>
+
                             @if ($mail->booking)
-                                <div class="flex items-center space-x-1">
-                                    <x-icon.calendar.index class="w-5 h-5 fill-current" />
-                                    <span>
-                                        {{ $mail->booking->activity }}
-                                        -
-                                        {{ localDate($mail->booking->start_at)->toFormattedDayDateString() }}
-                                    </span>
-                                </div>
+                                <x-icon.calendar.index class="w-5 h-5 fill-current" />
+                                <span>
+                                    {{ $mail->booking->activity }}
+                                    -
+                                    {{ localDate($mail->booking->start_at)->toFormattedDayDateString() }}
+                                </span>
                             @else
-                                {{ $mail->to }}
+                                <span class="block truncate">{{ $mail->to }}</span>
                             @endif
-                        </td>
-                        <td class="px-3 py-3">
+                        </p>
+
+                        <p class="flex items-center space-x-1">
+                            <dfn class="not-italic font-medium">@lang('From'):</dfn>
+
                             @if ($mail->user)
-                                <div class="flex items-center space-x-1">
-                                    <x-icon.user-solid-square class="w-5 h-5 fill-current" />
-                                    <span>{{ $mail->user->name }} &lt;{{ $mail->user->email }}&gt;</span>
-                                </div>
+                                <x-icon.user-solid-square class="w-5 h-5 fill-current" />
+                                <span>{{ $mail->user->name }} &lt;{{ $mail->user->email }}&gt;</span>
                             @else
-                                {{ $mail->from }}
+                                <span class="block truncate">{{ $mail->from }}</span>
                             @endif
-                        </td>
-                        <td class="px-3 py-2">{{ localDate($mail->created_at) }}</td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="2" class="px-3 py-2">@lang('No mail found.')</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+                        </p>
+
+                        <p class="flex items-center space-x-1">
+                            <dfn class="not-italic font-medium">@lang('Received'):</dfn>
+
+                            <span>{{ localDate($mail->created_at) }}</span>
+                        </p>
+                    </div>
+                </div>
+            @empty
+                <p class="px-3 py-2">@lang('No mail to see.')</p>
+            @endforelse
+        </div>
     </section>
 </x-layout.app>
