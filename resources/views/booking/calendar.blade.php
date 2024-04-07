652c83985df9a8484f717b2896f863ab771851af
Phill Sparks <me@phills.me.uk>
2024-04-07 16:45:21 +0100

    Style calendar header like sticky headers

--- resources/views/booking/calendar.blade.php
+++ resources/views/booking/calendar.blade.php
@@ -1,5 +1,5 @@
 <x-layout.app :title="__('Calendar')" assets="resources/js/booking/calendar.js">
-    <section class="p-4 sm:px-8 grow">
+    <section>
         <div id="fullcalendar"></div>
     </section>
 </x-layout.app>
