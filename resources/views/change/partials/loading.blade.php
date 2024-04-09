51bd1635225e90c6e1fb4e70388f938974efa683
Phill Sparks <me@phills.me.uk>
2024-04-09 22:50:01 +0100

    Fix change loading from being too wide on some screens

--- resources/views/change/partials/loading.blade.php
+++ resources/views/change/partials/loading.blade.php
@@ -1,8 +1,8 @@
 <div class="animate-pulse flex space-x-4">
     <div class="flex-1 space-y-2 py-1">
-        <div class="h-2 w-48 max-w-full bg-slate-200 rounded"></div>
+        <div class="h-2 w-full max-w-48 bg-slate-200 rounded"></div>
         <div class="border-l-2 ml-2 p-2 space-y-3">
-            <div class="h-2 w-96 max-w-full bg-slate-200 rounded"></div>
+            <div class="h-2 w-full max-w-96 bg-slate-200 rounded"></div>
         </div>
     </div>
 </div>
