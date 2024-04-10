144b4a9a4a93a723f2cf118966c8600af52e1aea
Phill Sparks <me@phills.me.uk>
2024-04-10 07:15:41 +0100

    Fix mail log loading from being too wide on some screens

--- resources/views/mail-log/partials/loading.blade.php
+++ resources/views/mail-log/partials/loading.blade.php
@@ -4,9 +4,9 @@
             <x-icon.loading class="w-5 h-5 fill-current animate-spin" />
         </p>
         <div class="flex-1 space-y-4 py-2">
-            <div class="h-2 w-96 max-w-full bg-slate-200 rounded"></div>
-            <div class="h-2 w-96 max-w-full bg-slate-200 rounded"></div>
-            <div class="h-2 w-48 max-w-full bg-slate-200 rounded"></div>
+            <div class="h-2 w-full max-w-96 bg-slate-200 rounded"></div>
+            <div class="h-2 w-full max-w-96 bg-slate-200 rounded"></div>
+            <div class="h-2 w-full max-w-48 bg-slate-200 rounded"></div>
         </div>
     </div>
 </div>
