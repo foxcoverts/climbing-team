d0125be166a233f656082a23fc9c073bfb27349f
Phill Sparks <me@phills.me.uk>
2024-04-10 09:05:22 +0100

    Fix textareas not setting correct initial size

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -5,7 +5,7 @@
         $el.style.height = '0px';
         $el.style.height = $el.scrollHeight + 'px'
     },
-}" x-init="resize" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+}" x-init="$nextTick(resize)" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
     'class' =>
         'border-gray-300 disabled:cursor-not-allowed disabled:text-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:disabled:text-gray-600 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm resize-y',
     'style' => 'color-scheme: light dark',
