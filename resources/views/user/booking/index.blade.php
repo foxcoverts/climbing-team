cd8aa7c3e208c9d852246e5c49b943dc0ad25bb1
Phill Sparks <me@phills.me.uk>
2024-04-14 17:55:19 +0100

    Fix broken user bookings list

--- resources/views/user/booking/index.blade.php
+++ resources/views/user/booking/index.blade.php
@@ -1,23 +1,21 @@
 <x-layout.app :title="__(':name - Bookings', ['name' => $user->name])">
     <section>
-        <header class="bg-white dark:bg-gray-800 border-b sm:sticky sm:top-0 sm:z-10 px-4 sm:px-8 ">
-            <div class="py-2 flex flex-wrap min-h-16 max-w-prose items-center justify-between gap-2">
+        <header class="bg-white dark:bg-gray-800 border-b sm:sticky sm:top-0 px-4 sm:px-8">
+            <div class="py-2 min-h-16 flex flex-wrap items-center justify-between gap-2 max-w-prose">
                 <h1 class="text-2xl font-medium text-gray-900 dark:text-gray-100">
                     @lang(':name - Bookings', ['name' => $user->name])
                 </h1>
 
                 @can('manage', App\Models\Booking::class)
-                    <nav class="grow flex justify-end">
-                        <x-button.primary :href="route('user.booking.invite', $user)">
-                            @lang('Invite')
-                        </x-button.primary>
+                    <nav class="flex items-center gap-4 justify-end grow">
+                        <x-button.primary :href="route('user.booking.invite', $user)" :label="__('Invite')" />
                     </nav>
                 @endcan
             </div>
         </header>
 
         <div class="p-4 sm:px-8 sm:mt-4">
-            @include('booking.partials.table-list', [
+            @include('booking.partials.list', [
                 'showRoute' => 'booking.show',
             ])
         </div>
