3faa7acbb5b479dfe60f8146c8bfccaf2c82de1c
Phill Sparks <me@phills.me.uk>
2024-04-14 17:45:55 +0100

    Improve flex wrap for Kit Check header on small screens

--- resources/views/kit-check/index.blade.php
+++ resources/views/kit-check/index.blade.php
@@ -1,13 +1,13 @@
 <x-layout.app :title="__('Latest Kit Checks')">
     <section x-data="{ showExpired: true }">
-        <header class="bg-white dark:bg-gray-800 border-b sm:sticky sm:top-0 sm:z-10 px-4 sm:px-8">
-            <div class="flex items-center justify-between max-w-prose">
-                <h1 class="text-2xl font-medium py-4 text-gray-900 dark:text-gray-100">
+        <header class="bg-white dark:bg-gray-800 border-b sm:sticky sm:top-0 px-4 sm:px-8">
+            <div class="py-2 min-h-16 flex flex-wrap items-center justify-between gap-2 max-w-prose">
+                <h1 class="text-2xl font-medium text-gray-900 dark:text-gray-100">
                     @lang('Latest Kit Checks')
                 </h1>
 
                 @can('create', App\Models\KitCheck::class)
-                    <nav>
+                    <nav class="flex items-center gap-4 justify-end grow">
                         <x-button.primary :href="route('kit-check.create')">@lang('Log Kit Check')</x-button.primary>
                     </nav>
                 @endcan
