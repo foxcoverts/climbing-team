2e054503b6c749cc476da912f8707648fb5dcb79
Phill Sparks <me@phills.me.uk>
2024-04-16 22:54:18 +0100

    Add missing z-10 for booking.edit

--- resources/views/booking/edit.blade.php
+++ resources/views/booking/edit.blade.php
@@ -14,7 +14,7 @@
             this.booking.confirmed = ev.target.checked;
         }
     }">
-        <header class="bg-white dark:bg-gray-800 border-b sm:sticky sm:top-0 px-4 sm:px-8">
+        <header class="bg-white dark:bg-gray-800 border-b sm:sticky sm:top-0 px-4 sm:px-8 sm:z-10">
             <div class="py-2 min-h-16 flex flex-wrap items-center justify-between gap-2 max-w-prose">
                 <h1 class="text-2xl font-medium text-gray-900 dark:text-gray-100">
                     <span x-text="booking.activity || 'Booking'">{{ $form->activity }}</span>
