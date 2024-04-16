4b5b0c1eae32907f8a80c150d073d6a863fa0a07
Phill Sparks <me@phills.me.uk>
2024-04-16 17:44:50 +0100

    Placeholder test for booking show page

---
+++ tests/Feature/ShowBookingTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ShowBookingTest extends TestCase
+{
+    public function test_booking_page_is_displayed(): void
+    {
+        $this->markTestIncomplete('Tests on individual bookings have not been written yet.');
+    }
+}
