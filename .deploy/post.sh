7d45bc827aa495641c891cd950cd55d51e725309
Phill Sparks <me@phills.me.uk>
2024-04-19 15:46:14 +0100

    Create webspace deploy script

---
+++ .deploy/post.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+echo "Running post-deploy scripts"
+
+# Ensure storage directories exist
+mkdir -p storage/app/public storage/framework/{cache,sessions,testing,views} storage/logs
+
+# Clear the old cache
+php8.2-cli artisan optimize:clear ||:
+
+# Run database migrations
+php8.2-cli artisan migrate --force -n
+
+# Setup storage links
+php8.2-cli artisan storage:link
+
+# Recreate cache
+php8.2-cli artisan optimize
+
+# Generate a new key
+php8.2-cli artisan key:generate --force -n
+
+# Exit maintenance mode
+php8.2-cli artisan up
+
+echo "Deployment finished!"
