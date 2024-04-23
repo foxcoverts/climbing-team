11d92e0cd1d3416ec4762cf7cc21033106ba9e64
Phill Sparks <me@phills.me.uk>
2024-04-23 12:24:21 +0100

    Schedule `queue:work` to run every 5 minutes

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('queue:work --stop-when-empty')
+    ->everyFiveMinutes()
+    ->withoutOverlapping()
+    ->emailOutputOnFailure('webmaster@climbfoxcoverts.co.uk');
