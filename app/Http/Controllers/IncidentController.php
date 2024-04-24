74c3556c7d92c147f3a3b85e22c089009edc7410
Phill Sparks <me@phills.me.uk>
2024-04-24 11:31:07 +0100

    Send email report to team leader, CC reporter

--- app/Http/Controllers/IncidentController.php
+++ app/Http/Controllers/IncidentController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\Role;
 use App\Forms\IncidentForm;
 use App\Http\Requests\StoreIncidentRequest;
+use App\Mail\IncidentReport;
 use App\Models\Incident;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class IncidentController extends Controller
 {
@@ -32,5 +37,20 @@ class IncidentController extends Controller
         Gate::authorize('create', Incident::class);
 
         $incident = new Incident($request->validated());
+
+        Mail::to($this->incidentReportRecipients())
+            ->cc($incident->reporterMailAddress())
+            ->send(new IncidentReport($incident));
+
+        return redirect()->route('dashboard')
+            ->with('alert.message', __('Thank-you. Your incident report has been submitted.'));
+    }
+
+    /**
+     * Get users who should receive incident reports.
+     */
+    protected function incidentReportRecipients(): Collection
+    {
+        return User::where('role', Role::TeamLeader)->select(['name', 'email'])->get();
     }
 }
--- app/Models/Incident.php
+++ app/Models/Incident.php
@@ -6,6 +6,7 @@ use App\Enums\Incident\Gender;
 use App\Enums\Incident\Injury;
 use App\Enums\Incident\MembershipType;
 use Carbon\Carbon;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Propaganistas\LaravelPhone\PhoneNumber;
@@ -41,9 +42,9 @@ class Incident
      */
     public ?Collection $injuries;
 
-    public bool $emergencyServices;
+    public ?bool $emergencyServices;
 
-    public bool $hospital;
+    public ?bool $hospital;
 
     public bool $damaged;
 
@@ -87,5 +88,82 @@ class Incident
         }
 
         return $this;
+    }
+
+    /**
+     * Confirm if any of the injuries are likely to be RIDDOR reportable.
+     *
+     * Anyone who was taken directly to hospital, or any of the named major injuries.
+     */
+    public function isRIDDOR(): bool
+    {
+        if ($this->hospital === true) {
+            return true;
+        }
+
+        if ($this->hasInjuries()) {
+            return collect($this->injuries)
+                ->reject(fn ($injury) => $injury == Injury::Minor)
+                ->isNotEmpty();
+        }
+
+        return false;
+    }
+
+    public function reporterMailAddress(): Address
+    {
+        return new Address($this->reporterEmail, $this->reporterName);
+    }
+
+    /* ----- has methods ------ */
+
+    public function hasInjuredName(): bool
+    {
+        return ! empty($this->injuredName);
+    }
+
+    public function hasInjuredDateOfBirth(): bool
+    {
+        return ! is_null($this->injuredDateOfBirth);
+    }
+
+    public function hasInjuredGender(): bool
+    {
+        return ! is_null($this->injuredGender);
+    }
+
+    public function hasGroupName(): bool
+    {
+        return ! empty($this->groupName);
+    }
+
+    public function hasContactPhone(): bool
+    {
+        return ! is_null($this->contactPhone);
+    }
+
+    public function hasContactAddress(): bool
+    {
+        return ! empty($this->contactAddress);
+    }
+
+    public function hasInjuries(): bool
+    {
+        return $this->injuries?->isNotEmpty() ?? false;
+    }
+
+    public function hasEmergencyServices(): bool
+    {
+        return ! is_null($this->emergencyServices);
+    }
+
+    public function hasHospital(): bool
+    {
+        return ! is_null($this->hospital);
+    }
+
+    public function hasFirstAid(): bool
+    {
+        return ! empty($this->firstAid);
     }
 }
