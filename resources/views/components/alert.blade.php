328518b386ebde3853041212801432f2ea261030
Phill Sparks <hello@phills.me.uk>
2024-05-03 21:03:06 +0100

    Prevent alert close X from being squashed

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -14,7 +14,7 @@
 @endphp
 @if (Arr::has($alert, 'message'))
     <div {{ $attributes->class([
-        'border-b mb-2 sm:mb-0 text-black px-4 sm:px-8 py-3 flex items-center',
+        'border-b mb-2 sm:mb-0 text-black px-4 sm:px-8 py-3 flex items-start',
         'bg-blue-100 border-blue-200' => $color == 'blue',
         'bg-red-100 border-red-200' => $color == 'red',
     ]) }}
@@ -33,15 +33,17 @@
             @endif
         </p>
 
-        <svg @class([
-            'fill-current h-6 w-6',
-            'text-blue-500' => $color == 'blue',
-            'text-red-500' => $color == 'red',
-        ]) role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-            @click="open = false">
-            <title>Close</title>
-            <path
-                d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-        </svg>
+        <div>
+            <svg @class([
+                'fill-current h-6 w-6',
+                'text-blue-500' => $color == 'blue',
+                'text-red-500' => $color == 'red',
+            ]) role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                @click="open = false">
+                <title>Close</title>
+                <path
+                    d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+        </div>
     </div>
 @endisset
