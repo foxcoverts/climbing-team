fb4eb022d7cfcefb0bdcda8d89dfa90273b71a71
Phill Sparks <hello@phills.me.uk>
2024-05-11 21:16:15 +0100

    Add loading 'progress' bar for fullcalendar

--- resources/js/booking/calendar.js
+++ resources/js/booking/calendar.js
@@ -5,6 +5,12 @@ import listPlugin from "@fullcalendar/list";
 import momentPlugin from "@fullcalendar/moment";
 import moment from "moment";
 
+const progressBarEl = document.createElement('div');
+progressBarEl.classList.add('h-2', 'transition');
+progressBarEl.style.opacity = 0;
+
+var progressBarInterval = null;
+
 const calendarEl = document.getElementById("fullcalendar");
 window.Calendar = new Calendar(calendarEl, {
     plugins: [dayGridPlugin, listPlugin, timeGridPlugin, momentPlugin],
@@ -64,6 +70,42 @@ window.Calendar = new Calendar(calendarEl, {
                 successCallback(res.data.data.map(transformEvent));
             })
             .catch(failureCallback);
+    },
+    loading(loadingState) {
+        if (loadingState) {
+            progressBarEl.dataset.percent = 0;
+            progressBarEl.style.transitionProperty = 'none';
+            progressBarEl.style.width = '0';
+            progressBarEl.classList.remove('bg-green-600', 'animate-pulse');
+            progressBarEl.classList.add('bg-blue-600');
+            progressBarEl.style.opacity = '1';
+
+            if (progressBarInterval != null) {
+                clearInterval(progressBarInterval);
+            }
+
+            progressBarInterval = setInterval(function () {
+                progressBarEl.dataset.percent = Math.min(100, parseInt(progressBarEl.dataset.percent) + Math.random() * 5);
+                progressBarEl.style.transitionProperty = 'background-color, opacity, width';
+                progressBarEl.style.width = progressBarEl.dataset.percent + '%';
+
+                if (progressBarEl.dataset.percent == 100) {
+                    clearInterval(progressBarInterval);
+                    progressBarInterval = null;
+
+                    progressBarEl.classList.add('animate-pulse');
+                }
+            }, 100);
+        } else {
+            progressBarEl.style.width = '100%';
+            progressBarEl.classList.add('bg-green-600');
+            progressBarEl.classList.remove('bg-blue-600', 'animate-pulse');
+            clearInterval(progressBarInterval);
+            progressBarInterval = setTimeout(function () {
+                progressBarEl.style.opacity = '0';
+                progressBarInterval = null;
+            }, 2000);
+        }
     },
     eventDidMount(info) {
         if (info.view.type == "timeGridDay") {
@@ -177,6 +219,15 @@ function onWindowResize() {
     ) {
         window.Calendar.changeView("dayGridMonth");
     }
+
+    if (window.innerWidth < 600) {
+        // Before calendar, below site header
+        calendarEl.parentNode.insertBefore(progressBarEl, calendarEl);
+    } else {
+        // After calendar toolbar, before calendar main
+        calendarEl.insertBefore(progressBarEl, calendarEl.getElementsByClassName('fc-view-harness')[0]);
+    }
 }
 
 window.Calendar.render();
+onWindowResize();
