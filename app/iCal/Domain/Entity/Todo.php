8d7342acf0a694892d5b129a32c7fb03612be760
Phill Sparks <hello@phills.me.uk>
2024-06-16 15:25:14 +0100

    Add Todo Organizer

--- app/iCal/Domain/Entity/Todo.php
+++ app/iCal/Domain/Entity/Todo.php
@@ -2,11 +2,14 @@
 
 namespace App\iCal\Domain\Entity;
 
+use Eluceo\iCal\Domain\ValueObject\Organizer;
 use Eluceo\iCal\Domain\ValueObject\UniqueIdentifier;
 
 class Todo
 {
     private UniqueIdentifier $uniqueIdentifier;
+
+    private ?Organizer $organizer = null;
 
     public function __construct(?UniqueIdentifier $uniqueIdentifier = null)
     {
@@ -16,5 +19,24 @@ class Todo
     public function getUniqueIdentifier(): UniqueIdentifier
     {
         return $this->uniqueIdentifier;
+    }
+
+    public function getOrganizer(): Organizer
+    {
+        assert($this->organizer !== null);
+
+        return $this->organizer;
+    }
+
+    public function setOrganizer(?Organizer $organizer): self
+    {
+        $this->organizer = $organizer;
+
+        return $this;
+    }
+
+    public function hasOrganizer(): bool
+    {
+        return $this->organizer !== null;
     }
 }
--- tests/Unit/iCal/Domain/Entity/TodoTest.php
+++ tests/Unit/iCal/Domain/Entity/TodoTest.php
@@ -3,8 +3,11 @@
 namespace Tests\Unit\iCal\Domain\Entity;
 
 use App\iCal\Domain\Entity\Todo;
+use Eluceo\iCal\Domain\ValueObject\EmailAddress;
+use Eluceo\iCal\Domain\ValueObject\Organizer;
 use Eluceo\iCal\Domain\ValueObject\UniqueIdentifier;
 use Tests\TestCase;
+use TypeError;
 
 class TodoTest extends TestCase
 {
@@ -22,5 +25,36 @@ class TodoTest extends TestCase
         $todo = new Todo(new UniqueIdentifier($uid));
 
         $this->assertEquals($uid, (string) $todo->getUniqueIdentifier());
+    }
+
+    public function test_set_get_organizer(): void
+    {
+        $name = fake()->name();
+        $email = fake()->email();
+        $todo = new Todo();
+
+        // setOrganizer is chainable
+        $this->assertEquals($todo, $todo->setOrganizer(new Organizer(new EmailAddress($email), $name)));
+        // hasOrganizer is true when any Organizer is set
+        $this->assertTrue($todo->hasOrganizer());
+
+        // getOrganizer returns the set organizer
+        $organizer = $todo->getOrganizer();
+        $this->assertEquals($email, $organizer->getEmailAddress()->getEmailAddress());
+        $this->assertEquals($name, $organizer->getDisplayName());
+    }
+
+    public function test_get_fails_with_no_organizer(): void
+    {
+        $this->expectException(TypeError::class);
+
+        $todo = new Todo();
+        $todo->getOrganizer();
+    }
+
+    public function test_has_organizer_with_no_organizer(): void
+    {
+        $todo = new Todo();
+        $this->assertFalse($todo->hasOrganizer());
     }
 }
