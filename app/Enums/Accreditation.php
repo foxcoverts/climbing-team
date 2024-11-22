259f63bf4643c0b679dfbd2c19cada182bc4813d
Phill Sparks <hello@phills.me.uk>
2024-11-22 14:22:39 +0000

    Add UserResource to admin panel

--- app/Enums/Accreditation.php
+++ app/Enums/Accreditation.php
@@ -2,10 +2,24 @@
 
 namespace App\Enums;
 
-enum Accreditation: string
+use Filament\Support\Colors\Color;
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+
+enum Accreditation: string implements HasColor, HasLabel
 {
     case KitChecker = 'kit-checker';
     case ManageBookings = 'manage-bookings';
     case ManageQualifications = 'manage-qualifications';
     case ManageUsers = 'manage-users';
+
+    public function getColor(): string|array|null
+    {
+        return Color::Yellow;
+    }
+
+    public function getLabel(): ?string
+    {
+        return __('app.user.accreditation.'.$this->value);
+    }
 }
