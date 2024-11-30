98943d3c198c0e06e9489d8c8204478bdd586600
Phill Sparks <hello@phills.me.uk>
2024-11-30 22:37:57 +0000

    Add basic KitCheckResource to admin panel

---
+++ app/Filament/Resources/KitCheckResource.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\Accreditation;
+use App\Enums\Role;
+use App\Filament\Resources\KitCheckResource\Pages;
+use App\Models\KitCheck;
+use Carbon\Carbon;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\Colors\Color;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class KitCheckResource extends Resource
+{
+    protected static ?string $model = KitCheck::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-check';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required()
+                    ->preload()
+                    ->searchable(),
+                Forms\Components\Select::make('checked_by_id')
+                    ->relationship(
+                        name: 'checked_by', titleAttribute: 'name',
+                        modifyQueryUsing: fn (Builder $query) => $query
+                            ->whereRaw('FIND_IN_SET(?, accreditations)', [Accreditation::KitChecker->value])
+                            ->orWhere('role', Role::TeamLeader)
+                    )
+                    ->default(fn (Request $request) => $request->user()->id)
+                    ->preload()
+                    ->searchable(),
+                Forms\Components\DatePicker::make('checked_on')
+                    ->default(Carbon::now())
+                    ->required(),
+                Forms\Components\Textarea::make('comment')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('checked_on')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->state(fn (KitCheck $record): string => match ($record->isExpired()) {
+                        true => 'Expired',
+                        false => 'Good',
+                    })
+                    ->badge()
+                    ->color(fn (KitCheck $record) => match ($record->isExpired()) {
+                        true => 'danger',
+                        false => Color::Lime,
+                    }),
+                Tables\Columns\TextColumn::make('checked_by.name')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('status')
+                    ->options([
+                        'good' => 'Good',
+                        'expired' => 'Expired',
+                    ])
+                    ->default('good')
+                    ->query(fn (Builder $query, array $data): Builder => match ($data['value']) {
+                        'good' => $query->whereDate('checked_on', '>=', Carbon::now()->subYear(1)),
+                        'expired' => $query->whereDate('checked_on', '<', Carbon::now()->subYear(1)),
+                        default => $query,
+                    }),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListKitChecks::route('/'),
+            'create' => Pages\CreateKitCheck::route('/create'),
+            'edit' => Pages\EditKitCheck::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/KitCheckResource/Pages/CreateKitCheck.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\KitCheckResource\Pages;
+
+use App\Filament\Resources\KitCheckResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateKitCheck extends CreateRecord
+{
+    protected static string $resource = KitCheckResource::class;
+}
