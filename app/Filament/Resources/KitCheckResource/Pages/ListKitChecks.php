826863a3befd9731f55d13b32afd23abf50bcc85
Phill Sparks <hello@phills.me.uk>
2024-11-30 22:37:57 +0000

    Add basic KitCheckResource to admin panel

---
+++ app/Filament/Resources/KitCheckResource/Pages/ListKitChecks.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\KitCheckResource\Pages;
+
+use App\Filament\Resources\KitCheckResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListKitChecks extends ListRecords
+{
+    protected static string $resource = KitCheckResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
