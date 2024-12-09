14a173135ba86e351d84907f18ba4fa0821bf749
Phill Sparks <hello@phills.me.uk>
2024-12-09 15:10:09 +0000

    Add DocumentResource to admin panel

---
+++ app/Filament/Resources/DocumentResource/Pages/ViewDocument.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\DocumentResource\Pages;
+
+use App\Filament\Resources\DocumentResource;
+use App\Models\Document;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Support\Facades\Storage;
+
+class ViewDocument extends ViewRecord
+{
+    protected static string $resource = DocumentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('download')
+                ->color('info')
+                ->icon('heroicon-m-arrow-down-tray')
+                ->action(fn (Document $record) => Storage::response($record->file_path, $record->file_name)),
+            Actions\EditAction::make(),
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
