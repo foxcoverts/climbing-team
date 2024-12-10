d7208cc0cd1c692d9e0b51eb666b6b39ef175456
Phill Sparks <hello@phills.me.uk>
2024-12-10 10:14:21 +0000

    Move Key transfer logic into model

--- app/Filament/Resources/KeyResource/Pages/EditKey.php
+++ app/Filament/Resources/KeyResource/Pages/EditKey.php
@@ -2,11 +2,9 @@
 
 namespace App\Filament\Resources\KeyResource\Pages;
 
-use App\Events\KeyTransferred;
 use App\Filament\Resources\KeyResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
-use Illuminate\Database\Eloquent\Model;
 
 class EditKey extends EditRecord
 {
@@ -17,19 +15,5 @@ class EditKey extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
-    }
-
-    protected function handleRecordUpdate(Model $record, array $data): Model
-    {
-        $lastHolder = $record->holder;
-
-        $record->update($data);
-
-        if ($record->wasChanged('holder_id')) {
-            $record->refresh();
-            event(new KeyTransferred($record, from: $lastHolder));
-        }
-
-        return $record;
     }
 }
