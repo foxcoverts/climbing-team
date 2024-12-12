2ec34bd3e07310192347d505ebb5866b41d47d8f
Phill Sparks <hello@phills.me.uk>
2024-12-12 15:13:08 +0000

    Add MailLogResource to admin panel

--- app/Models/Calendar/Attendee.php
+++ app/Models/Calendar/Attendee.php
@@ -3,45 +3,97 @@
 namespace App\Models\Calendar;
 
 use App\Enums\BookingAttendeeStatus;
+use App\Models\Concerns\HasNoDatabase;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
 use Sabre\VObject\Property;
 
-class Attendee
+class Attendee extends Model
 {
-    public function __construct(
-        protected Property $vattendee
-    ) {
+    use HasNoDatabase;
+
+    protected Property $vattendee;
+
+    public static function fromVAttendee(Property $vattendee): static
+    {
+        $attendee = new static;
+        $attendee->vattendee = $vattendee;
+
+        return $attendee;
     }
 
+    protected function user(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => User::findByEmail($this->email)
+        );
+    }
+
+    /**
+     * @deprecated use `user` attribute instead
+     */
     public function getUser(): ?User
     {
-        return User::findByEmail($this->getEmail());
+        return $this->user;
     }
 
+    protected function email(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => str_replace('mailto:', '', $this->vattendee->getValue())
+        );
+    }
+
+    /**
+     * @deprecated use `email` attribute instead
+     */
     public function getEmail(): string
     {
-        return str_replace('mailto:', '', $this->vattendee->getValue());
+        return $this->email;
     }
 
+    public function status(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => match ((string) $this->vattendee['PARTSTAT']) {
+                'ACCEPTED' => BookingAttendeeStatus::Accepted,
+                'DECLINED' => BookingAttendeeStatus::Declined,
+                'NEEDS-ACTION' => BookingAttendeeStatus::NeedsAction,
+                'TENTATIVE' => BookingAttendeeStatus::Tentative,
+                default => null, // Unsupported: 'DELEGATED'
+            }
+        );
+    }
+
+    /**
+     * @deprecated use `status` attribute instaed
+     */
     public function getStatus(): ?BookingAttendeeStatus
     {
-        return match ((string) $this->vattendee['PARTSTAT']) {
-            'ACCEPTED' => BookingAttendeeStatus::Accepted,
-            'DECLINED' => BookingAttendeeStatus::Declined,
-            'NEEDS-ACTION' => BookingAttendeeStatus::NeedsAction,
-            'TENTATIVE' => BookingAttendeeStatus::Tentative,
-            default => null, // Unsupported: 'DELEGATED'
-        };
+        return $this->status;
     }
 
+    public function comment(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                if (isset($this->vattendee['X-RESPONSE-COMMENT'])) {
+                    return html_entity_decode(
+                        str_replace('\;', ';', (string) $this->vattendee['X-RESPONSE-COMMENT'])
+                    );
+                }
+
+                return null;
+            }
+        );
+    }
+
+    /**
+     * @deprecated use `comment` attribute instead
+     */
     public function getComment(): ?string
     {
-        if (isset($this->vattendee['X-RESPONSE-COMMENT'])) {
-            return html_entity_decode(
-                str_replace('\;', ';', (string) $this->vattendee['X-RESPONSE-COMMENT'])
-            );
-        }
-
-        return null;
+        return $this->comment;
     }
 }
