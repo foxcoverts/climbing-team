6c41980ff222707b12af3cb4e567c88425b4f97f
Phill Sparks <hello@phills.me.uk>
2024-12-15 14:41:16 +0000

    Add Kit Checks to UserResource

---
+++ app/Filament/Resources/UserResource/RelationManagers/KitChecksRelationManager.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use App\Filament\Resources\KitCheckResource;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class KitChecksRelationManager extends RelationManager
+{
+    protected static string $relationship = 'kitChecks';
+
+    public function form(Form $form): Form
+    {
+        return KitCheckResource::form($form);
+    }
+
+    public function table(Table $table): Table
+    {
+        return KitCheckResource::table($table)
+            ->recordTitleAttribute('checked_on')
+            ->headerActions([
+                Tables\Actions\CreateAction::make()
+                    ->createAnother(false),
+            ]);
+    }
+}
