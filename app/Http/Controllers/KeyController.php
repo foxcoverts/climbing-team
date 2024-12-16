80edb612d8b08bca25afce006327806966baba52
Phill Sparks <hello@phills.me.uk>
2024-12-16 15:00:24 +0000

    Remove Key CUD from front-end
    Moves Transfer code into Key edit/update.

--- app/Http/Controllers/KeyController.php
+++ app/Http/Controllers/KeyController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreKeyRequest;
 use App\Http\Requests\UpdateKeyRequest;
 use App\Models\Key;
 use App\Models\User;
@@ -27,91 +26,37 @@ class KeyController extends Controller
     }
 
     /**
-     * Show the form for creating a new key.
+     * Show the form for transferring the key to another holder.
      */
-    public function create(Request $request): View
+    public function edit(Request $request, Key $key): View
     {
-        Gate::authorize('create', Key::class);
+        Gate::authorize('transfer', $key);
 
-        $count = Key::count();
-        $key = new Key([
-            'name' => sprintf('Key %d', $count + 1),
-        ]);
-        $key->holder()->associate($request->user());
-
-        return view('key.create', [
+        return view('key.edit', [
+            'ajax' => $request->ajax(),
             'key' => $key,
             'users' => User::orderBy('name')->get(),
         ]);
     }
 
-    /**
-     * Store a newly created key in storage.
-     */
-    public function store(StoreKeyRequest $request): RedirectResponse
-    {
-        Gate::authorize('create', Key::class);
-
-        $key = Key::create($request->validated());
-
-        return redirect()->route('key.index')
-            ->with('alert.message', __(':Name added.', ['name' => $key->name]));
-    }
-
-    /**
-     * Display the specified key.
-     */
-    public function show(Key $key): View
-    {
-        Gate::authorize('view', $key);
-
-        return view('key.show', [
-            'key' => $key,
-        ]);
-    }
-
-    /**
-     * Show the form for editing the specified key.
-     */
-    public function edit(Request $request, Key $key): View
-    {
-        Gate::authorize('update', $key);
-
-        return view('key.edit', [
-            'ajax' => $request->ajax(),
-            'key' => $key,
-        ]);
-    }
-
-    /**
-     * Update the specified key in storage.
-     */
     public function update(UpdateKeyRequest $request, Key $key): RedirectResponse
     {
-        Gate::authorize('update', $key);
+        Gate::authorize('transfer', $key);
 
-        $key->update($request->validated());
+        $key->holder()->associate($request->holder_id);
+        $key->save();
 
-        if ($key->wasChanged()) {
+        if ($key->wasChanged('holder_id')) {
             return redirect()->route('key.index')
                 ->with('alert', [
-                    'message', __('Key updated.'),
+                    'message' => __(':Key transferred to :holder.', [
+                        'key' => $key->name,
+                        'holder' => $key->holder->name,
+                    ]),
                     '$dispatch' => 'key:updated',
                 ]);
         } else {
             return redirect()->route('key.index');
         }
-    }
-
-    /**
-     * Remove the specified key from storage.
-     */
-    public function destroy(Key $key): RedirectResponse
-    {
-        Gate::authorize('delete', $key);
-
-        $key->delete();
-
-        return redirect()->route('key.index')->with('alert.message', __('Key deleted.'));
     }
 }
