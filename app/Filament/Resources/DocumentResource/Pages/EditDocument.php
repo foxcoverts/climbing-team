f09ed72ce8ddd1d185602978f521790c8e30b9ae
Phill Sparks <hello@phills.me.uk>
2024-12-20 14:36:15 +0000

    Add ActivityLog to DocumentResource

--- app/Filament/Resources/DocumentResource/Pages/EditDocument.php
+++ app/Filament/Resources/DocumentResource/Pages/EditDocument.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\DocumentResource\Pages;
 use App\Filament\Resources\DocumentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use RalphJSmit\Filament\Activitylog;
 
 class EditDocument extends EditRecord
 {
@@ -13,6 +14,9 @@ class EditDocument extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Activitylog\Actions\TimelineAction::make()
+                ->label('Log')
+                ->color('info'),
             Actions\DeleteAction::make(),
             Actions\ForceDeleteAction::make(),
             Actions\RestoreAction::make(),
