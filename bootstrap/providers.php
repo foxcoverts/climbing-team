d46074271386a5e2e39f3476ebf8f6bf71bad055
Phill Sparks <hello@phills.me.uk>
2024-12-20 20:32:13 +0000

    Log a custom 'kitChecked' event on users when a kit check is created
    Note: Although there is a 'kit_check_id' property noted, if the KitCheck
    is deleted the log will remain.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,7 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\CarbonServiceProvider::class,
     App\Providers\FakerServiceProvider::class,
+    App\Providers\PossessiveStringProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\TimelineProvider::class,
 ];
