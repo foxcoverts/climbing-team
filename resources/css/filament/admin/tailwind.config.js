7e2baf7f322401246e1e076f3c6e1187f0999618
Phill Sparks <hello@phills.me.uk>
2024-12-20 14:22:15 +0000

    Add ActivityLog to KeyResource

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -6,5 +6,6 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        './vendor/ralphjsmit/laravel-filament-activitylog/resources/**/*.blade.php',
     ],
 }
