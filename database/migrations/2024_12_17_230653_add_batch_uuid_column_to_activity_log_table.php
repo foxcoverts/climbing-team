04fd5f20a11f2b7b429b1ba39ecfa6538810b2ac
Phill Sparks <hello@phills.me.uk>
2024-12-21 10:35:54 +0000

    Use CHAR instead of UUID for batch_uuid

--- database/migrations/2024_12_17_230653_add_batch_uuid_column_to_activity_log_table.php
+++ database/migrations/2024_12_17_230653_add_batch_uuid_column_to_activity_log_table.php
@@ -1,15 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddBatchUuidColumnToActivityLogTable extends Migration
 {
     public function up()
     {
+        // We have MariaDB 10.6 in production, but MariaDB did not introduce the UUID type until 10.7.
+        // Laravel already uses the UUID for MariaDB so here we are forcing an equivalent type instead.
         Schema::connection(config('activitylog.database_connection'))->table(config('activitylog.table_name'), function (Blueprint $table) {
-            $table->uuid('batch_uuid')->nullable()->after('properties');
+            $table->char('batch_uuid', 36)->nullable()->after('properties');
         });
     }
 
