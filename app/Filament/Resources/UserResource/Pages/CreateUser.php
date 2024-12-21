0d2dc3f5803291ba504cc32f43a325d35b04e338
Phill Sparks <hello@phills.me.uk>
2024-12-21 20:32:28 +0000

    Create new Users with blank password, and send invites

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -2,11 +2,23 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Events\Registered;
 use App\Filament\Resources\UserResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['password'] = '';
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        event(new Registered($this->record));
+    }
 }
