4f40b7625363201b3c6be5025ea72f9e16a294d9
Phill Sparks <hello@phills.me.uk>
2024-12-21 12:35:32 +0000

    Improve display of Recent News widget

--- resources/views/filament/widgets/recent-news.blade.php
+++ resources/views/filament/widgets/recent-news.blade.php
@@ -1,29 +1,42 @@
+@use(App\Filament\Resources\NewsPostResource)
+@use(App\Models\NewsPost)
 <x-filament-widgets::widget>
     <x-filament::section icon="heroicon-o-newspaper">
         <x-slot name="heading">
-            <x-filament::modal id="recent-news" width="2xl" sticky-header>
-                <x-slot name="trigger">
-                    {{ $title }}
-                </x-slot>
-            
-                <x-slot name="heading">
-                    {{ $title }}
-                </x-slot>
-                
-                <div class="prose dark:prose-invert max-w-prose">
-                    <p>{{ __(':Author posted :ago', compact('ago', 'author')) }}.</p>
-
-                    <x-markdown :text="$body" />
-                </div>
-
-                <x-slot name="footerActions">
-                    <x-filament::link :href="route('news.index')">{{ __('View all news') }}</x-filament::link>
-                </x-slot>
-            </x-filament::modal>
+            {{ __('Recent News') }}
         </x-slot>
 
-        {!! $summary !!}
+        <div class="space-y-4">
+            <h2 class="font-medium text-xl">{{ $title }}</h2>
 
-        <p><x-filament::link :href="$link" wire:click.prevent="$dispatch('open-modal', { id: 'recent-news' })">{{ __('Read more...') }}</x-filament::link></p>
+            {!! $summary !!}
+
+            <x-filament::grid default="2">
+                <x-filament::grid.column>
+                    <em class="text-sm">{{ __('Posted :ago.', compact('ago')) }}</em>
+                </x-filament::grid.column>
+                <x-filament::grid.column class="text-right">
+                    <x-filament::link wire:click="$dispatch('open-modal', { id: 'recent-news' })" icon="heroicon-m-sparkles" class="cursor-pointer">{{ __('Read more...') }}</x-filament::link>
+                </x-filament::grid.column>
+            </x-filament::grid>
+        </div>
     </x-filament::section>
+
+    <x-filament::modal id="recent-news" width="2xl" sticky-footer>
+        <x-slot name="heading">
+            <h1 class="font-medium text-3xl">{{ $title }}</h1>
+        </x-slot>
+
+        <div class="prose dark:prose-invert max-w-prose">
+            <x-markdown :text="$body" />
+        </div>
+
+        <x-slot name="footerActions">
+            <div class="text-sm grow"><x-markdown>{{ __('Posted :ago by **:author**.', compact('ago', 'author')) }}</x-markdown></div>
+
+            @can('viewAny', NewsPost::class)
+                <x-filament::link :href="NewsPostResource::getUrl('index')" wire:navigate>{{ __('View all news') }}</x-filament::link>
+            @endcan
+        </x-slot>
+    </x-filament::modal>
 </x-filament-widgets::widget>
