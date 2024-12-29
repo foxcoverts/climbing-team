2a374269f0e7310513707a2eb459384ff20f9591
Phill Sparks <me@phills.me.uk>
2024-12-29 12:08:14 +0000

    Show a 'Forbidden' error when a cluster is not accessible

--- app/Filament/Clusters/Admin.php
+++ app/Filament/Clusters/Admin.php
@@ -3,8 +3,15 @@
 namespace App\Filament\Clusters;
 
 use Filament\Clusters\Cluster;
+use Illuminate\Contracts\View\View;
 
 class Admin extends Cluster
 {
     protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    public function render(): View
+    {
+        // Will only get here if there are no accessible resources/pages.
+        abort(403);
+    }
 }
