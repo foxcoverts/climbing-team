d0ab7db1c474a8f74b7ae177f1379c9bf050ffc0
Phill Sparks <me@phills.me.uk>
2024-12-29 12:08:14 +0000

    Show a 'Forbidden' error when a cluster is not accessible

--- app/Filament/Clusters/Developer.php
+++ app/Filament/Clusters/Developer.php
@@ -3,8 +3,15 @@
 namespace App\Filament\Clusters;
 
 use Filament\Clusters\Cluster;
+use Illuminate\Contracts\View\View;
 
 class Developer extends Cluster
 {
     protected static ?string $navigationIcon = 'heroicon-o-server-stack';
+
+    public function render(): View
+    {
+        // Will only get here if there are no accessible resources/pages.
+        abort(403);
+    }
 }
