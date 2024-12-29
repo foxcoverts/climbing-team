aa805e1edd70c1ed378152f8cbaa9b5e96d79054
Phill Sparks <me@phills.me.uk>
2024-12-29 12:13:57 +0000

    Add My Keys Resource

--- app/Filament/Clusters/Admin/Resources/KeyResource/Pages/ListKeys.php
+++ app/Filament/Clusters/Admin/Resources/KeyResource/Pages/ListKeys.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Clusters\Admin\Resources\KeyResource\Pages;
 
 use App\Filament\Clusters\Admin\Resources\KeyResource;
+use App\Filament\Clusters\My;
 use App\Filament\Pages\Concerns\HasClusterSidebarNavigation;
+use App\Models\Key;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Gate;
 
 class ListKeys extends ListRecords
 {
@@ -16,6 +19,12 @@ class ListKeys extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('my')
+                ->label('My Keys')
+                ->link()
+                ->icon(My::getNavigationIcon())
+                ->url(fn (): string => My\Resources\KeyResource::getUrl())
+                ->visible(fn (): bool => Gate::check('viewOwn', Key::class)),
             Actions\CreateAction::make(),
         ];
     }
