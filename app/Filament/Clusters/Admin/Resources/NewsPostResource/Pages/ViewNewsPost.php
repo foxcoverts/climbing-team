f5e6db55d1c90d71b145985ed730de88b900d027
Phill Sparks <me@phills.me.uk>
2024-12-29 17:21:33 +0000

    Add team-wide news resource
    - Restricts access to the admin resource
    - Labeled as 'news'
    - Updates the recent news widget link

--- app/Filament/Clusters/Admin/Resources/NewsPostResource/Pages/ViewNewsPost.php
+++ app/Filament/Clusters/Admin/Resources/NewsPostResource/Pages/ViewNewsPost.php
@@ -4,8 +4,10 @@ namespace App\Filament\Clusters\Admin\Resources\NewsPostResource\Pages;
 
 use App\Filament\Clusters\Admin\Resources\NewsPostResource;
 use App\Filament\Pages\Concerns\HasClusterSidebarNavigation;
+use App\Filament\Resources\NewsPostResource as TeamNewsPostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Contracts\Support\Htmlable;
 use RalphJSmit\Filament\Activitylog;
 
 class ViewNewsPost extends ViewRecord
@@ -13,6 +15,19 @@ class ViewNewsPost extends ViewRecord
     use HasClusterSidebarNavigation;
 
     protected static string $resource = NewsPostResource::class;
+
+    public function getHeading(): string|Htmlable
+    {
+        return $this->getRecordTitle();
+    }
+
+    public function getSubheading(): string|Htmlable|null
+    {
+        $link = TeamNewsPostResource::getUrl('view', ['record' => $this->getRecord()]);
+        $path = parse_url($link, PHP_URL_PATH);
+
+        return str("[$path]($link)")->inlineMarkdown()->toHtmlString();
+    }
 
     protected function getHeaderActions(): array
     {
