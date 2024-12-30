de30c0b2f95c97714bf2fac5650e159b607a40a9
Phill Sparks <me@phills.me.uk>
2024-12-30 12:34:04 +0000

    Add "Edit Profile" page in "My" cluster

---
+++ resources/views/filament/clusters/my/pages/edit-profile.blade.php
@@ -0,0 +1,7 @@
+<x-filament-panels::page>
+    <x-filament-panels::form wire:submit="save">
+        {{ $this->form }}
+
+        <x-filament-panels::form.actions :actions="$this->getFormActions()" />
+    </x-filament-panels::form>
+</x-filament-panels::page>
