e169b3f9d9b76c1c1363e03bb74bb655906f58c2
Phill Sparks <me@phills.me.uk>
2024-12-31 20:15:39 +0000

    Implement best practice with ChangePassword page
    - Use generic `getRecord()` function for consistency
    - Set the form's model and operation
    - Add key bindings to Save action

--- resources/views/filament/clusters/my/pages/change-password.blade.php
+++ resources/views/filament/clusters/my/pages/change-password.blade.php
@@ -2,6 +2,8 @@
     <x-filament-panels::form wire:submit="save">
         {{ $this->form }}
 
-        <x-filament-panels::form.actions :actions="$this->getFormActions()" />
+        <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" />
     </x-filament-panels::form>
+
+    <x-filament-panels::page.unsaved-data-changes-alert />
 </x-filament-panels::page>
