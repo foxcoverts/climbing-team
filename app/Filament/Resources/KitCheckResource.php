0ae75b01479b7e01260bf4ee5bb8bf7dd079a327
Phill Sparks <me@phills.me.uk>
2025-01-01 10:05:03 +0000

    Move KitCheckResource from My Cluster

--- app/Filament/Resources/KitCheckResource.php
+++ app/Filament/Resources/KitCheckResource.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Filament\Clusters\My\Resources;
+namespace App\Filament\Resources;
 
 use App\Filament\Clusters\Admin\Resources\KitCheckResource as AdminKitCheckResource;
-use App\Filament\Clusters\My\Resources\KitCheckResource\Pages;
+use App\Filament\Resources\KitCheckResource\Pages;
 use App\Models\KitCheck;
 use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -19,13 +20,6 @@ class KitCheckResource extends Resource
 
     protected static ?string $activeNavigationIcon = 'heroicon-s-document-check';
 
-    protected static ?int $navigationSort = 3;
-
-    public static function getEloquentQuery(): Builder
-    {
-        return parent::getEloquentQuery()->whereBelongsTo(Auth::user());
-    }
-
     public static function table(Table $table): Table
     {
         return AdminKitCheckResource::table($table)
@@ -38,6 +32,21 @@ class KitCheckResource extends Resource
     public static function infolist(Infolist $infolist): Infolist
     {
         return AdminKitCheckResource::infolist($infolist);
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->whereBelongsTo(Auth::user());
+    }
+
+    public static function canViewAny(): bool
+    {
+        return static::can('viewOwn');
+    }
+
+    public static function authorizeViewAny(): void
+    {
+        static::authorize('viewOwn');
     }
 
     public static function getPages(): array
