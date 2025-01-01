d43251d1da37bef1e4d8a3da56a0e6ba1b0c9f14
Phill Sparks <me@phills.me.uk>
2025-01-01 10:00:50 +0000

    Move KeyResource from My Cluster

--- app/Filament/Resources/KeyResource/Pages/ListKeys.php
+++ app/Filament/Resources/KeyResource/Pages/ListKeys.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace App\Filament\Clusters\My\Resources\KeyResource\Pages;
+namespace App\Filament\Resources\KeyResource\Pages;
 
 use App\Filament\Clusters\Admin;
-use App\Filament\Pages\Concerns\HasClusterSidebarNavigation;
-use App\Filament\Clusters\My\Resources\KeyResource;
+use App\Filament\Resources\KeyResource;
 use App\Models\Key;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
@@ -12,8 +11,6 @@ use Illuminate\Support\Facades\Gate;
 
 class ListKeys extends ListRecords
 {
-    use HasClusterSidebarNavigation;
-
     protected static string $resource = KeyResource::class;
 
     protected function getHeaderActions(): array
