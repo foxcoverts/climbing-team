3a8951ac03c70639f8b8eee7e8b2629991430659
Phill Sparks <me@phills.me.uk>
2025-01-03 22:41:24 +0000

    Add public News preview
    Including meta tags for og and twitter. Removes News from v1 Dashboard.

--- app/Filament/Resources/NewsPostResource/Pages/ViewNewsPost.php
+++ app/Filament/Resources/NewsPostResource/Pages/ViewNewsPost.php
@@ -6,12 +6,52 @@ use App\Filament\Clusters\Admin;
 use App\Filament\Resources\NewsPostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Gate;
 
 class ViewNewsPost extends ViewRecord
 {
     protected static string $resource = NewsPostResource::class;
+
+    public function render(): View
+    {
+        $record = $this->getRecord();
+        $title = $record->title;
+        $description = $record->summaryText;
+        $updated = localDate($record->updated_at)->toIso8601String();
+        $image = asset('images/news/fox-coverts-climbing-necker.png');
+        $image_width = 744;
+        $image_height = 328;
+
+        FilamentView::registerRenderHook(
+            PanelsRenderHook::HEAD_END,
+            fn (): string => implode('', [
+                '<meta property="og:site_name" content="'.config('app.name', 'Climbing Team').'" />',
+                '<meta property="og:title" content="'.$title.'" />',
+                '<meta property="twitter:title" content="'.$title.'" />',
+                '<meta name="description" content="'.$description.'" />',
+                '<meta property="og:description" content="'.$description.'" />',
+                '<meta name="twitter:description" content="'.$description.'" />',
+                '<meta property="og:updated_time" content="'.$updated.'" />',
+
+                '<meta property="og:image" content="'.$image.'" />',
+                '<meta name="twitter:image" content="'.$image.'" />',
+                '<meta property="og:image:width" content="'.$image_width.'" />',
+                '<meta property="og:image:height" content="'.$image_height.'" />',
+
+                '<meta property="og:type" content="website" />',
+                '<meta property="og:locale" content="'.config('app.locale', 'en').'" />',
+                '<meta name="twitter:card" content="summary" />',
+                '<meta name="twitter:site" content="@FoxCoverts" />',
+            ]),
+            scopes: ViewNewsPost::class,
+        );
+
+        return parent::render();
+    }
 
     public function getHeading(): string|Htmlable
     {
