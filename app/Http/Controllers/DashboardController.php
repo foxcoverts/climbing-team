2f6bf2fd3fb3306739c414517faad4d9a0a533e0
Phill Sparks <me@phills.me.uk>
2025-01-03 22:41:24 +0000

    Add public News preview
    Including meta tags for og and twitter. Removes News from v1 Dashboard.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Enums\BookingAttendeeStatus;
 use App\Models\Booking;
-use App\Models\NewsPost;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
@@ -27,12 +26,9 @@ class DashboardController extends Controller
             ])
             ->ordered()->first();
 
-        $newsPost = NewsPost::orderByDesc('created_at')->first();
-
         return view('dashboard', [
             'next' => $nextBooking,
             'invite' => $invite,
-            'post' => $newsPost,
         ]);
     }
 }
