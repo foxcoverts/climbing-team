1809195e01f9ff32c67e20ef6b4cb88b18d57e59
Phill Sparks <me@phills.me.uk>
2025-01-22 20:53:11 +0000

    Remove 'inactive' from badges and add to name column

--- resources/css/filament/team/tailwind.config.js
+++ resources/css/filament/team/tailwind.config.js
@@ -8,5 +8,6 @@ export default {
         './vendor/filament/**/*.blade.php',
         './vendor/ralphjsmit/laravel-filament-activitylog/resources/**/*.blade.php',
         './vendor/ralphjsmit/laravel-filament-onboard/resources/**/*.blade.php',
+        './vendor/awcodes/filament-badgeable-column/resources/**/*.blade.php',
     ],
 }
