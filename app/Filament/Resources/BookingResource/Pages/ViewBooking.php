4dceff644651bdcd5317e5ac774b9a3334290651
Phill Sparks <me@phills.me.uk>
2025-01-25 22:42:53 +0000

    Edit link from base view booking page

--- app/Filament/Resources/BookingResource/Pages/ViewBooking.php
+++ app/Filament/Resources/BookingResource/Pages/ViewBooking.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Resources\BookingResource\Pages;
 
+use App\Filament\Clusters\Admin;
 use App\Filament\Resources\BookingResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Support\Facades\Gate;
 
 class ViewBooking extends ViewRecord
 {
@@ -11,6 +14,13 @@ class ViewBooking extends ViewRecord
 
     protected function getHeaderActions(): array
     {
-        return [];
+        return [
+            Actions\ActionGroup::make([
+                Actions\Action::make('edit')
+                    ->icon('heroicon-m-pencil-square')
+                    ->url(fn (): string => Admin\Resources\BookingResource::getUrl('edit', ['record' => $this->getRecord()]))
+                    ->visible(fn (): bool => Gate::check('update', $this->getRecord())),
+            ]),
+        ];
     }
 }
