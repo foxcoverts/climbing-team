b64ecfdd16e2a5e97347af9dfff4786d4b2ebc06
Phill Sparks <me@phills.me.uk>
2025-02-02 18:35:31 +0000

    Remove v1 rota

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,26 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\BookingAttendeeStatus;
-use App\Models\Booking;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    public function __invoke(Request $request): View
+    public function __invoke(): View
     {
-        Gate::authorize('viewOwn', Booking::class);
-
-        $nextBooking = Booking::future()->notCancelled()
-            ->attendeeStatus($request->user(), [
-                BookingAttendeeStatus::Accepted, BookingAttendeeStatus::Tentative,
-            ])
-            ->ordered()->first();
-
-        return view('dashboard', [
-            'next' => $nextBooking,
-        ]);
+        return view('dashboard');
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,2 @@
 <x-layout.app :title="__('Dashboard')">
-    <div class="divide-y">
-        @include('dashboard.partials.next-widget', [
-            'booking' => $next,
-            'icon' => 'inbox.check',
-            'title' => __('Next Booking'),
-            'route' => 'booking.show',
-            'more' => [
-                'route' => 'booking.rota',
-                'label' => __('View your rota'),
-            ],
-        ])
-    </div>
 </x-layout.app>
