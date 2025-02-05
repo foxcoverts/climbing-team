6007b02f256c27fd31539c0a30ded9f82dd61948
Phill Sparks <me@phills.me.uk>
2025-02-05 17:39:53 +0000

    Remove extra route definition for privacy policy

--- app/Filament/Pages/PrivacyPolicy.php
+++ app/Filament/Pages/PrivacyPolicy.php
@@ -3,12 +3,22 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use Filament\Panel;
 
 class PrivacyPolicy extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
+    protected static ?string $activeNavigationIcon = 'heroicon-s-document-text';
+
     protected static ?string $navigationGroup = 'Policies';
 
     protected static string $view = 'filament.pages.privacy-policy';
+
+    protected static string|array $withoutRouteMiddleware = ['auth'];
+
+    public static function isEmailVerificationRequired(Panel $panel): bool
+    {
+        return false;
+    }
 }
